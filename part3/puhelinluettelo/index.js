56dbb0939809fe150888b6182a97b8ade160cf9b
vekkul1 <veeti.hallaaho@gmail.com>
2025-09-22 14:24:41 +0300

    fix

--- part3/puhelinluettelo/index.js
+++ part3/puhelinluettelo/index.js
@@ -106,7 +106,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log("server running on port:", PORT)
 })
