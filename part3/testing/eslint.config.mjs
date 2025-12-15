f3a2ca88823b393fed1fe063e82784a0b085ed59
vekkul1 <veeti.hallaaho@gmail.com>
2025-12-15 02:48:11 +0200

    part3 finish

--- part3/testing/eslint.config.mjs
+++ part3/testing/eslint.config.mjs
@@ -2,7 +2,6 @@ import globals from 'globals'
 import js from '@eslint/js'
 import stylisticJs from '@stylistic/eslint-plugin'
 
-
 export default [
   js.configs.recommended,
   {
@@ -20,9 +19,14 @@ export default [
       '@stylistic/js/linebreak-style': ['error', 'unix'],
       '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
     }, 
   },
   {
-    ignores: ['dist/**'],
+    ignores: ['dist/**']
   },
 ]
--- part3/testing/index.js
+++ part3/testing/index.js
@@ -12,92 +12,92 @@ app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('body', req => {
-    return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-      response.json(notes)
-    })
+  Note.find({}).then(notes => {
+    response.json(notes)
   })
+})
 
 app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+  const body = request.body
     
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
 
-    note.save()
-        .then(savedNote => {
-            response.json(savedNote)
-        })
-        .catch(error => next(error))
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const { content, important } = request.body
+  const { content, important } = request.body
 
-    Note.findById(request.params.id)
-        .then(note => {
-            if (!note) {
-                return response.status(404).end()
-            }
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
 
-            note.content = content
-            note.important  = important
+      note.content = content
+      note.important  = important
 
-            return note.save().then((updatedNote) => {
-                response.json(updatedNote)
-            })
-        })
-        .catch(error => next(error))
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
 }) 
 
 const unkownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unkown endpoint' })
+  response.status(404).send({ error: 'unkown endpoint' })
 }
 
 app.use(unkownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
   
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id!' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id!' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
   
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
+  console.log(`Server running on ${PORT}`)
 })
