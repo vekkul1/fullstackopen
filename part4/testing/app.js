d27c5913800dee02ad2d8c1619bbfb120390f508
vekkul1 <veeti.hallaaho@gmail.com>
2025-12-23 14:06:26 +0200

    part4b testing

--- part4/testing/app.js
+++ part4/testing/app.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middlewares')
 
 const app = express()
 
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
-  .connect(config.MONGODB_URI, { family: 4 })
+  .connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
@@ -19,5 +20,11 @@ mongoose
 
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/notes', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
 module.exports = app
--- part4/testing/utils/config.js
+++ part4/testing/utils/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ quiet: true })
 
 const PORT = process.env.PORT
 
