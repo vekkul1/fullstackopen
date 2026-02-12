459c68edfa9689f2e32e27f2015db196ce7295dc
vekkul1 <veeti.hallaaho@gmail.com>
2026-02-12 14:38:51 +0200

    5.16w

--- part5/bloglist-frontend/src/components/BlogForm.test.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.test.jsx
@@ -1,18 +1,40 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import BlogForm  from './BlogForm'
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import BlogForm from "./BlogForm";
 
-describe('<BlogForm />', () => {
-  test('test createBlog', () => {
-    const createBlog = vi.fn()
-    render(<BlogForm createBlog={createBlog} />)
-    
-    const user = userEvent.setup()
-    const button = screen.getByText('create')
-    screen.debug(button)
-    user.click(button)
-    console.log(createBlog)
-    expect(createBlog.mock.calls).toHaveLength(1)
-    
- })
-})
+describe("<BlogForm />", () => {
+  test("test createBlog called 1 time", async () => {
+    const createBlog = vi.fn();
+    render(<BlogForm createBlog={createBlog} />);
+
+    const user = userEvent.setup();
+    const createButton = screen.getByText("create");
+    await user.click(createButton);
+
+    expect(createBlog.mock.calls).toHaveLength(1);
+  });
+  test("test createBlog called with right inforamtion", async () => {
+    const createBlog = vi.fn();
+    render(<BlogForm createBlog={createBlog} />);
+
+    const blog = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    };
+
+    const user = userEvent.setup();
+    const titleField = screen.getByLabelText("title:");
+    const authorField = screen.getByLabelText("author:");
+    const urlField = screen.getByLabelText("url:");
+    const createButton = screen.getByText("create");
+
+    await user.type(titleField, blog.title);
+    await user.type(authorField, blog.author);
+    await user.type(urlField, blog.url);
+    await user.click(createButton);
+
+    expect(createBlog.mock.calls).toHaveLength(1);
+    assert.deepEqual(createBlog.mock.calls[0][0], blog);
+  });
+});
