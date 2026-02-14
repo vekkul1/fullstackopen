1d768a9aeb6ee4b616b5dc5b2140887a2df2e841
vekkul1 <veeti.hallaaho@gmail.com>
2026-02-15 00:56:56 +0200

    5d

--- part5/testing/e2e/playwright.config.js
+++ part5/testing/e2e/playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -13,41 +13,42 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+  timeout: 3000,
+  fullyParallel: false,
+  workers: 1,
+  testDir: "./tests",
   /* Run tests in files in parallel */
-  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
@@ -78,4 +79,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
---
+++ part5/testing/e2e/tests/note_app.spec.js
@@ -0,0 +1,67 @@
+const { describe, test, expect, beforeEach } = require("@playwright/test");
+const { loginWith, createNote } = require("./helper");
+
+describe("Note app", () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("/api/testing/reset");
+    await request.post("/api/users", {
+      data: {
+        name: "rooter",
+        username: "root",
+        password: "sekret",
+      },
+    });
+
+    await page.goto("");
+  });
+  test("front page can be opened", async ({ page }) => {
+    const locator = page.getByText("Notes");
+    await expect(locator).toBeVisible();
+    await expect(
+      page.getByText(
+        "Note app, Department of Computer Science, University of Helsinki 2026",
+      ),
+    ).toBeVisible();
+  });
+
+  test("user can log in", async ({ page }) => {
+    await loginWith(page, "root", "sekret");
+
+    await expect(page.getByText("logged in as rooter!")).toBeVisible();
+  });
+
+  test("login fails with wrong password", async ({ page }) => {
+    await loginWith(page, "root", "wrong");
+
+    const errorDiv = page.locator(".error");
+    await expect(errorDiv).toContainText("wrong credentials");
+    await expect(errorDiv).toHaveCSS("border-style", "solid");
+    await expect(errorDiv).toHaveCSS("color", "rgb(255, 0, 0)");
+
+    await expect(page.getByText("logged in as rooter")).not.toBeVisible();
+  });
+
+  describe("when logged in!", () => {
+    beforeEach(async ({ page }) => {
+      loginWith(page, "root", "sekret");
+    });
+
+    test("a new note can be created", async ({ page }) => {
+      await createNote(page, "a note created by playwright");
+      await expect(
+        page.getByText("a note created by playwright"),
+      ).toBeVisible();
+    });
+
+    describe("and a note exists", () => {
+      beforeEach(async ({ page }) => {
+        await createNote(page, "a note created by playwright");
+      });
+
+      test("importance can be changed", async ({ page }) => {
+        await page.getByRole("button", { name: "make not important" }).click();
+        await expect(page.getByText("make important")).toBeVisible();
+      });
+    });
+  });
+});
