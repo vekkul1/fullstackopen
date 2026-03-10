2283a76fe4bfe9e0570c888418b1bf5b14f67bd0
vekkul1 <veeti.hallaaho@gmail.com>
2026-03-10 20:50:54 +0200

    6d react query tests

--- part6/testing/src/App.jsx
+++ part6/testing/src/App.jsx
@@ -1,21 +1,63 @@
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-
-import NoteForm from './components/NoteForm'
-import Notes from './components/Notes'
-import VisibilityFilter from './components/VisibilityFilter'
-import { initializeNotes } from './reducers/noteReducer'
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query'
+import { getNotes, createNote, updateNote } from './requests'
 
 const App = () => {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(initializeNotes(dispatch))
-  }, [dispatch])
+  const queryClient = useQueryClient()
+
+  const newNoteMutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData(['notes'])
+      queryClient.setQueryData(['notes'], notes.concat(newNote))
+    },
+  })
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    },
+  })
+
+  const addNote = async (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    newNoteMutation.mutate({ content, important: true })
+    console.log(content)
+  }
+
+  const toggleImportance = (note) => {
+    updateNoteMutation.mutate({ ...note, important: !note.important })
+    console.log('toggle importance of', note.id)
+  }
+
+  const result = useQuery({
+    queryKey: ['notes'],
+    queryFn: getNotes,
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const notes = result.data
+
   return (
     <div>
-      <NoteForm />
-      <VisibilityFilter />
-      <Notes />
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      {notes.map((note) => (
+        <li key={note.id} onClick={() => toggleImportance(note)}>
+          {note.content}
+          <strong> {note.important ? 'important' : ''}</strong>
+        </li>
+      ))}
     </div>
   )
 }
--- part6/testing/src/main.jsx
+++ part6/testing/src/main.jsx
@@ -1,11 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import App from './App'
-import store from './store'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </Provider>,
+  </QueryClientProvider>,
 )
